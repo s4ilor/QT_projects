aa6ea4348e7b2d237e187fc70da16a236f244307
Paweł Olbrycht <36730911+s4ilor@users.noreply.github.com>
2018-10-25 02:04:15 +0200

    Update tempconverter.h

--- git/tempconverter.h
+++ git/tempconverter.h
@@ -1,3 +1,18 @@
+/*! Klasa TempConverter ma dwa zastosowania - pierwsze to oczywiscie automatyczna konwersja skali temperatury (C->F, F->C),
+a drugie to zmiana typu danych do kontrolki QLCDNumber.
+Klasa ta posiada:
+* sloty do ustawiania temperatury (uzytkownik korzysta z QDial)
+* sygnaly do przesylania informacji o jakichkolwiek zmianach
+* metody pomocnicze do pobierania temperatury w konkretnej skali
+* zmienna prywatna do przechowywania wartosci aktualnej temperatury
+
+Paweł Olbrycht, pazdziernik 2018
+
+*/
+
+
+
+
 #ifndef TEMPCONVERTER_H
 #define TEMPCONVERTER_H
 
@@ -9,25 +24,25 @@ class TempConverter : public QObject
 public:
     explicit TempConverter(int tempCelsius, QObject *parent = 0);
 
-    int tempCelsius() const;               //getter
-    int tempFahrenheit() const;           //
+    int tempCelsius() const;               ///< pobranie temp. w skali C
+    int tempFahrenheit() const;           ///< pobranie temp. w skali F
 
 signals:
-    void tempCelsiusChanged(int);
-    void tempFahrenheitChanged(int);
+    void tempCelsiusChanged(int);           ///< sygnal zmiany wartosci temp. w skali C
+    void tempFahrenheitChanged(int);        ///< sygnal zmiany wartosci temp. w skali F
 
-    void typeCelsiusChanged(double);
-    void typeFahrenheitChanged(double);
+    void typeCelsiusChanged(double);        ///< sygnal zmiany typu danej w skali C
+    void typeFahrenheitChanged(double);     ///< sygnal zmiany typu danej w skali F
 
 public slots:
-    void setTempCelsius(int);           //setter - temp
-    void setTempFahrenheit(int);       //
+    void setTempCelsius(int);           ///< setter dla temperatury w skali C
+    void setTempFahrenheit(int);       ///< oraz w skali F
 
-    void setTypeCelsius(int);        //setter - type
-    void setTypeFahrenheit(int);    //
+    void setTypeCelsius(int);        ///< setter dla typu danych w skali C
+    void setTypeFahrenheit(int);    ///< oraz w skali F
 
 private:
-    int m_tempCelsius;
+    int m_tempCelsius;              ///< zmienna prywatna przechowujaca temperature
 };
 
 #endif // TEMPCONVERTER_H
